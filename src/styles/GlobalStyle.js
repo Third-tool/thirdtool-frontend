9ea8f78e4fd39258e1fc017e3d7bf93580999c26
choyeon <cho1428hee@naver.com>
2025-05-04 00:24:22 +0900

    feat: 개발 기본환경 셋팅 및 globalstyle 설정

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+    }
+
+    body {
+    background-color: #000;
+    color: #fff;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    font-family: 'Pretendard-Regular';
+    }
+
+    * {
+        box-sizing: border-box;
+        color: inherit; /* 하위 요소에도 폰트 색상 상속 */
+    }
+`
+
+export default GlobalStyle;
